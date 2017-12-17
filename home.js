01cdbb8a000ce793ea78f7d26e9f113c8b4613b9
Brian Silverman <bsilver16384@gmail.com>
2017-12-16 23:41:10 -0500

    Pledge and such

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
+// I pledge my honor that I have abided by the Stevens Honor System.
+//   - Brian Silverman
+//   - Nathaniel Blakely
+//   - Michael Watson
+//
+// CS-546 Final Project
 
 "use strict";
 const express = require('express');
